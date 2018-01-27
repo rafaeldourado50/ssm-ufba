bf2be216d902e446e03b9f2074cdc5b864a6766f
rafael.dourado810 <rafael.dourado810@gmail.com>
2018-01-26 19:53:33 -0500

    Ajustes no template do menu.

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -7,16 +7,16 @@
   }
 </style>
 <div class="col-md-3">
-    <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#"><i class="fa fa-home fa-fw"></i>Home</a></li>
-        <li><a href="{{ url('/areas') }}"><i class="fa fa-list-alt fa-fw"></i>Areas</a></li>
-        <li><a href="{{ url('/courses') }}"><i class="fa fa-bar-chart-o fa-fw"></i>Courses</a></li>
-        <li><a href="{{ url('/course_disciplines') }}"><i class="fa fa-book fa-fw"></i>Course Disciplines</a></li>
-        <li><a href="{{ url('/disciplines') }}"><i class="fa fa-file-o fa-fw"></i>Disciplines</a></li>
-        <li><a href="{{ url('/discipline_class_offers') }}"><i class="fa fa-pencil fa-fw"></i>Discipline Class Offers</a></li>
-        <li><a href="{{ url('/discipline_classes') }}"><i class="fa fa-cogs fa-fw"></i>Discipline Classes</a></li>       
-        <li><a href="{{ url('/professors') }}"><i class="fa fa-tasks fa-fw"></i>Professors</a></li>
-        <li><a href="{{ url('/schedules') }}"><i class="fa fa-calendar fa-fw"></i>Schedules</a></li>
-        <li><a href="{{ url('/students') }}"><i class="fa fa-table fa-fw"></i>Students</a></li>
-    </ul>
+  <ul class="nav nav-pills nav-stacked">
+    <li class="active"><a href="#"><i class="fa fa-home fa-fw"></i>Home</a></li>
+    <li><a href="{{ url('/areas') }}"><i class="fa fa-list-alt fa-fw"></i>Areas</a></li>
+    <li><a href="{{ url('/courses') }}"><i class="fa fa-bar-chart-o fa-fw"></i>Courses</a></li>
+    <li><a href="{{ url('/course_disciplines') }}"><i class="fa fa-book fa-fw"></i>Course Disciplines</a></li>
+    <li><a href="{{ url('/disciplines') }}"><i class="fa fa-file-o fa-fw"></i>Disciplines</a></li>
+    <li><a href="{{ url('/discipline_class_offers') }}"><i class="fa fa-pencil fa-fw"></i>Discipline Class Offers</a></li>
+    <li><a href="{{ url('/discipline_classes') }}"><i class="fa fa-cogs fa-fw"></i>Discipline Classes</a></li>       
+    <li><a href="{{ url('/professors') }}"><i class="fa fa-tasks fa-fw"></i>Professors</a></li>
+    <li><a href="{{ url('/schedules') }}"><i class="fa fa-calendar fa-fw"></i>Schedules</a></li>
+    <li><a href="{{ url('/students') }}"><i class="fa fa-table fa-fw"></i>Students</a></li>
+  </ul>
 </div>
