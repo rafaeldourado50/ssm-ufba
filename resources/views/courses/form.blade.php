00f920f9816e82bdb329bba6d0da091d0b7b9dce
rafael.dourado810 <rafael.dourado810@gmail.com>
2018-01-26 19:52:47 -0500

    Ajustes no CRUD de Cursos.

--- resources/views/courses/form.blade.php
+++ resources/views/courses/form.blade.php
@@ -1,5 +1,5 @@
 <div class="row {{ $errors->has('code') ? 'has-error' : ''}}">
-  <label for="name" class="col-md-2 control-label">{{ 'Name' }}</label>
+  <label for="name" class="col-md-4 control-label">{{ 'Name' }}</label>
   <div class="col-md-12">
     <input class="form-control" name="name" type="text" id="name" value="{{ $course->name or ''}}" >
     {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
@@ -7,7 +7,7 @@
 </div>
 
 <div class="row {{ $errors->has('code') ? 'has-error' : ''}}">
-  <label for="code" class="col-md-2 control-label">{{ 'Code' }}</label>
+  <label for="code" class="col-md-4 control-label">{{ 'Code' }}</label>
   <div class="col-md-12">
     <input class="form-control" name="code" type="text" id="code" value="{{ $course->code or ''}}" >
     {!! $errors->first('code', '<p class="help-block">:message</p>') !!}
@@ -15,14 +15,14 @@
 </div>
 
 <div class="row">
-  <label for="curriculum" class="col-md-2 control-label">{{ 'Curriculum' }}</label>
+  <label for="curriculum" class="col-md-4 control-label">{{ 'Curriculum' }}</label>
   <div class="col-md-12">
     <input class="form-control" name="curriculum" type="text" id="curriculum" value="{{ $course->curriculum or ''}}" >
   </div>
 </div>
 
 <div class="row">
-  <label for="area" class="col-md-2 control-label">{{ 'Area' }}</label>
+  <label for="area" class="col-md-4 control-label">{{ 'Area' }}</label>
   <div class="col-md-12">
     {!! Form::select('area_id', $areas, $valueAreaSelected, ['class' => 'form-control', 'placeholder' => 'Choose an area...']) !!}
   </div>
