ec956eef5562acc24de2b169ccf20ee51e4d91f8
rafael.dourado810 <rafael.dourado810@gmail.com>
2018-01-31 19:10:10 -0500

    Ajustes no layout. Unificando templates.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
   <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-md-12">
       <div class="panel panel-default">
         <div class="panel-heading">Login</div>
         <div class="panel-body">
+          
           <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
             {{ csrf_field() }}
 
@@ -62,5 +62,4 @@
       </div>
     </div>
   </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
     </div>
   </nav>
 
-  <div class="container-fluid">
+  <div class="container">
     @yield('content')
   </div>
 
